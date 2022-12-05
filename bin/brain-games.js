9e859efa57b87f7a5b5edb7d076ad7c8a6de3410
nikitakozlovjr <nikita3726898@gmail.com>
2022-12-05 14:23:25 +0300

    Добавили приветствие с игроком

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,6 @@
-console.log("Welcome to the Brain Games!");
+//Импорты
+import readlineSync from 'readline-sync';
+//код
+console.log("Welcome to the Brain Games!");//dsdjlbv 
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
