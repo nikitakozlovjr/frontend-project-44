62c87b1423534837c7e50c88f3374a62acded36e
nikitakozlovjr <nikita3726898@gmail.com>
2022-12-16 12:45:37 +0300

    Переписали игру, за счет импорта функций из index.js и game-logic-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,58 +1,30 @@
 #!/usr/bin/env node
 
 // Импорты
-import readlineSync from 'readline-sync';
+import { sayHi, outputCongratulations, checkAnswer } from '../src/index.js';
+import { calcLogig } from '../games/game-logik-calc.js';
 
-// Код
 // приветствие
-
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-
+const sayHiResult = sayHi();
 // выводим правила игры
-
 console.log('What is the result of the expression?');
+// внешние переменные для игрового цикла
+
+let rightAnswers = 0;// переменная, хранящая число правильных ответов игрока
+const fail = false;// переменная "СТОП-СЛОВО" при ее значении true цикл заканчивает свою работу
+let calcLogigResult;// в эту переменную поместим правильный ответ (результат calcLogig())
 
 // игровой цикл
-
-let rightAnswers = 0;
-
 while (rightAnswers < 3) {
-  // генерация двух случайных чисел
-  const fistNum = Math.floor(Math.random() * 100);
-  const secondNum = Math.floor(Math.random() * 100);
-  // генерация случайного индекса
-  const index = Math.floor(Math.random() * 3);
-  // массив, содержащий всевозможные операнды
-  const arrOfOperant = ['*', '+', '-'];
-  // составляем выражение для пользователя
-  const expression = `${String(fistNum)} ${arrOfOperant[index]} ${String(secondNum)}`;
-  // инициализируем переменную, которая будет содержать правильный ответ
-  let trueAnswer = 0;
-  // помещаем правильный ответ в переменую
-  if (index === 0) { // если index случайным образом оказался 0
-    trueAnswer = fistNum * secondNum;
-  } else if (index === 1) { // если index случайным образом оказался 1
-    trueAnswer = fistNum + secondNum;
-  } else if (index === 2) { // если index случайным образом оказался 2
-    trueAnswer = fistNum - secondNum;
-  }
-  // выводим пользователю выражение, которое он должен решить
-  console.log(`Question: ${expression}`);
-  // просим ввести его ответ и сразуже сохраняем его в переменной playerAnswer
-  const playerAnswer = readlineSync.question('Your answer: ');
-  // делаем проверку результата
-  if (Number(playerAnswer) === trueAnswer) {
-    console.log('Correct!');
-    rightAnswers += 1;
-  } else {
-    console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.)`);
-    console.log(`Let's try again, ${name}!`);
+  calcLogigResult = calcLogig();// выполняем функцию
+  const checkAnswerResult = checkAnswer(calcLogigResult, sayHiResult, fail);
+
+  if (checkAnswerResult === true) { // завершаем игру если checkAnswer() возвращает true
     break;
+  } else { // во всех других случаях увеличиваем кол-во правильных ответов на 1;
+    rightAnswers += 1;
   }
 }
 
-if (rightAnswers === 3) {
-  console.log(`Congratulations, ${name}!`);
-}
+// Выводим поздравление игроку, если он ответил три раза правильно
+outputCongratulations(sayHiResult, rightAnswers);
